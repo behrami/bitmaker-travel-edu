a1757e574404a36d1feb7b3dc6114252d7b27c10
behrami <behram1993@gmail.com>
2018-05-22 16:26:25 -0400

    fixed links

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,16 @@ Rails.application.routes.draw do
 
   root 'months#index'
 
-  # resources :hotels, only: %i[index show]
-
   resources :months, only: %i[index] do
     resources :countries, only: %i[index]
   end
 
   resources :countries, only: %i[index] do
     resources :cities, only: %i[index]
+  end
+
+  resources :cities, only: %i[index] do
+    resources :hotels, only: %i[index show]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
