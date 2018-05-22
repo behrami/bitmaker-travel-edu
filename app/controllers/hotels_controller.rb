fdf2bc7a6176d74997af671753914e6c7780e86f
behrami <behram1993@gmail.com>
2018-05-22 17:49:52 -0400

    extracted needed info from google api for one example

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -4,25 +4,30 @@ class HotelsController < ApplicationController
     @city_id = params[:city_id]
     @city_name = City.find(@city_id).name
 
-    
+    @country_id = City.find_by(name: @city_name).country_id
+    @country_name = Country.find(@country_id).name
 
-    hotelList_response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{@city_name}+hotel&key=#{key}")
+    hotelList_response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{@city_name}+#{@country_name}+hotel&key=#{key}")
     hotelList_body = JSON.parse(hotelList_response.body)
 
-    # @place_id = hotelList_body['results']['id']
+    @place_id = hotelList_body['results'][0]['place_id']
     @name = hotelList_body['results'][0]['name']
     @address = hotelList_body['results'][0]['formatted_address']
+    @rating = hotelList_body['results'][0]['rating']
+    @photo_reference = hotelList_body['results'][0]['photos'][0]['photo_reference']
+    @latitude = hotelList_body['results'][0]['geometry']['location']['lat']
+    @longitude = hotelList_body['results'][0]['geometry']['location']['lng']
 
     respond_to do |format|
       format.html
       format.json {  render json: {
-                                    # "placeID": pokemon_id,
+                                    "place_id": @place_id,
                                     "name": @name,
                                     "address": @address,
-                                    # "longitude": gify_url,
-                                    # "latitude": gify_url,
-                                    # "rating": ,
-                                    # "photo_reference":
+                                    "longitude": @longitude,
+                                    "latitude": @latitude,
+                                    "rating": @rating,
+                                    "photo_reference": @photo_reference
                                   }
                   }
     end
