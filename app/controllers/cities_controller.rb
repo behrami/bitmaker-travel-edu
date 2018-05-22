e951e4e3f5546bf12a898471c5c016ecc959aaca
behrami <behram1993@gmail.com>
2018-05-22 16:22:07 -0400

    fixed toronto not showing up

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,6 @@
 class CitiesController < ApplicationController
   def index
+    @country_id = params[:country_id]
+    @cities = Country.find(@country_id).cities
   end
 end
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,12 +1,6 @@
 class CountriesController < ApplicationController
-# before_action :load_country, only: [:index]
-
   def index
     @month_id = params[:month_id]
     @countries = Month.find(@month_id).countries
   end
-
-  # def load_country
-  #   @country = Country.find_by(month_id: params[:id])
-  # end
 end
