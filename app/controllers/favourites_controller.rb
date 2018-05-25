5966a751c1d3847dc2acde4fe877b5c0f4816f44
Sclarke5 <sclarke5555@gmail.com>
2018-05-25 12:26:32 -0400

    save hotels to database from show page

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,11 +3,31 @@ class FavouritesController < ApplicationController
   end
 
   def new
-    @favourite = Favourite.new 
+    @favourite = Favourite.new
   end
 
   def create
     @favourite = Favourite.new
+    @key = ENV['GOOGLE_KEY']
+
+    @hotel_id = params[:id]
+
+    @city_id = params[:city_id]
+    @city_name = City.find(@city_id).name
+
+    @country_id = City.find_by(name: @city_name).country_id
+    @country_name = Country.find(@country_id).name
+
+    hotelList_response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{@city_name}+#{@country_name}+hotel&key=#{@key}")
+    hotelList_body = JSON.parse(hotelList_response.body)
+    @hotelListArray = hotelList_body['results']
+
+    @foundHotelsArray= []
+    @hotelListArray.each do |hotel|
+      if hotel['place_id'] == @hotel_id
+        @foundHotelsArray.push(hotel)
+      end
+    end
   end
 
   def destroy
