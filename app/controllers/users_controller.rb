30e6bb4661c10ccfb048e6e0fe66f71b7c465f57
behrami <behram1993@gmail.com>
2018-05-27 08:57:31 -0400

    fixed sign in and up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path
     else
+      flash.now[:alert] = 'Unsuccessful Sign Up'
       render :new
     end
   end
