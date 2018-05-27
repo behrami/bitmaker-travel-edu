10556947aa90301aa6502fe5477ef13af79c9892
behrami <behram1993@gmail.com>
2018-05-27 08:16:11 -0400

    favourites fully functional with multiple users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root 'months#index'
+
   resource :user, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
+  resource :session, only: %i[new create destroy]
 
   resources :months, only: %i[index] do
     resources :countries, only: %i[index]
@@ -16,8 +17,9 @@ Rails.application.routes.draw do
     resources :hotels, only: %i[index show]
   end
 
+  resources :favourites, only: %i[index destroy]
   resources :hotels, only: %i[show] do
-    resources :favourites, only: %i[index new create destroy]
+    resources :favourites, only: %i[new create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
