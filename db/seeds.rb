ab2bb972e434a5111d58ee8c083e274330f4ed5b
behrami <behram1993@gmail.com>
2018-05-27 10:53:55 -0400

    added title

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create([
-                    {first_name: 'admin', last_name: 'admin', user_name:'admin', email: 'admin@admin.com', password: 'admin'}
+                    {first_name: 'admin', last_name: 'admin', user_name:'admin', email: 'admin@admin.com', password: 'admin'},
+                    {first_name: 'admin2', last_name: 'admin2', user_name:'admin2', email: 'admin2@admin.com', password: 'admin2'}
   ])
 
  months = Month.create([  {name: 'January'},
@@ -47,8 +48,11 @@ countries = Country.create([{ name: 'Canada'},
                             { name: 'Mexico'},
                             { name: 'Hungary'},
                             { name: 'Malaysia'},
-                            { name: 'Israel'}
-
+                            { name: 'Israel'},
+                            { name: 'Croatia'},
+                            { name: 'New Zealand'},
+                            { name: 'Argentina'},
+                            { name: 'Oman'}
                             ])
 
 cities = City.create([     {name: 'Toronto', country_id: 1},
@@ -76,6 +80,8 @@ cities = City.create([     {name: 'Toronto', country_id: 1},
                             {name: 'Kyoto', country_id: 8},
                             {name: 'Osaka', country_id: 8},
                             {name: 'Singapore', country_id: 9},
+                            {name: 'Jurong East', country_id: 9},
+                            {name: 'Bukit Batok', country_id: 9},
                             {name: 'Sydney', country_id: 10},
                             {name: 'Melbourne', country_id: 10},
                             {name: 'Perth', country_id: 10},
@@ -120,7 +126,19 @@ cities = City.create([     {name: 'Toronto', country_id: 1},
                             {name: 'Malacca', country_id: 23},
                             {name: 'Jerusalem', country_id: 24},
                             {name: 'Tel Aviv', country_id: 24},
-                            {name: 'Tiberias', country_id: 24}
+                            {name: 'Tiberias', country_id: 24},
+                            {name: 'Dubrovnik', country_id: 25},
+                            {name: 'Rovinj', country_id: 25},
+                            {name: 'Zagreb', country_id: 25},
+                            {name: 'Auckland', country_id: 26},
+                            {name: 'Wellington', country_id: 26},
+                            {name: 'Christchurch', country_id: 26},
+                            {name: 'Buenos Aires', country_id: 27},
+                            {name: 'Bariloche', country_id: 27},
+                            {name: 'Mar del Plata', country_id: 27},
+                            {name: 'Muscat', country_id: 28},
+                            {name: 'Salalah', country_id: 28},
+                            {name: 'Sur', country_id: 28},
                             ])
 
 Month.find_by(name: 'January').countries << (Country.find_by(name: 'Italy'))
@@ -158,3 +176,39 @@ Month.find_by(name: 'June').countries << (Country.find_by(name: 'USA'))
 Month.find_by(name: 'June').countries << (Country.find_by(name: 'Mexico'))
 Month.find_by(name: 'June').countries << (Country.find_by(name: 'Brazil'))
 Month.find_by(name: 'June').countries << (Country.find_by(name: 'Portugal'))
+
+Month.find_by(name: 'July').countries << (Country.find_by(name: 'Canada'))
+Month.find_by(name: 'July').countries << (Country.find_by(name: 'Italy'))
+Month.find_by(name: 'July').countries << (Country.find_by(name: 'Greece'))
+Month.find_by(name: 'July').countries << (Country.find_by(name: 'Spain'))
+Month.find_by(name: 'July').countries << (Country.find_by(name: 'Portugal'))
+
+Month.find_by(name: 'August').countries << (Country.find_by(name: 'Mexico'))
+Month.find_by(name: 'August').countries << (Country.find_by(name: 'Hungary'))
+Month.find_by(name: 'August').countries << (Country.find_by(name: 'Vietnam'))
+Month.find_by(name: 'August').countries << (Country.find_by(name: 'Spain'))
+Month.find_by(name: 'August').countries << (Country.find_by(name: 'Portugal'))
+
+Month.find_by(name: 'September').countries << (Country.find_by(name: 'Malaysia'))
+Month.find_by(name: 'September').countries << (Country.find_by(name: 'Israel'))
+Month.find_by(name: 'September').countries << (Country.find_by(name: 'USA'))
+Month.find_by(name: 'September').countries << (Country.find_by(name: 'Spain'))
+Month.find_by(name: 'September').countries << (Country.find_by(name: 'France'))
+
+Month.find_by(name: 'October').countries << (Country.find_by(name: 'Ireland'))
+Month.find_by(name: 'October').countries << (Country.find_by(name: 'Italy'))
+Month.find_by(name: 'October').countries << (Country.find_by(name: 'Greece'))
+Month.find_by(name: 'October').countries << (Country.find_by(name: 'Croatia'))
+Month.find_by(name: 'October').countries << (Country.find_by(name: 'South Africa'))
+
+Month.find_by(name: 'November').countries << (Country.find_by(name: 'Brazil'))
+Month.find_by(name: 'November').countries << (Country.find_by(name: 'USA'))
+Month.find_by(name: 'November').countries << (Country.find_by(name: 'Spain'))
+Month.find_by(name: 'November').countries << (Country.find_by(name: 'New Zealand'))
+Month.find_by(name: 'November').countries << (Country.find_by(name: 'Argentina'))
+
+Month.find_by(name: 'December').countries << (Country.find_by(name: 'New Zealand'))
+Month.find_by(name: 'December').countries << (Country.find_by(name: 'Oman'))
+Month.find_by(name: 'December').countries << (Country.find_by(name: 'Australia'))
+Month.find_by(name: 'December').countries << (Country.find_by(name: 'Singapore'))
+Month.find_by(name: 'December').countries << (Country.find_by(name: 'South Africa'))
