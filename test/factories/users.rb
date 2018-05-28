21557065d1a3a767a94930d0a5bf809aca23784e
behrami <behram1993@gmail.com>
2018-05-28 12:10:07 -0400

    removed pictures for now

---
+++ test/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    first_name "Behram"
+    last_name "I"
+    user_name "behram"
+    email "behram@gmail.com"
+    password "password"
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_that_user_can_be_created
+
+    # john = nil
+    # john = create(:user, first_name: 'John', last_name:'Bertlik')
+
+    behram=build(:user)
+    assert_equal(behram.first_name, 'Behram')
+  end
 end
